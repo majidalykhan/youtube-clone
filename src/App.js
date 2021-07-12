d60cc0e753bd251eb9098accf275a242577ccc5b
Majid Ali Khan <51267704+majidalykhan@users.noreply.github.com>
2021-07-12 17:18:25 +0500

    Grid Structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
 
+import { Grid } from "@material-ui/core";
+
+import youtube from "./api/youtube";
+
 class App extends React.Component {
   render() {
-    return <h1>Youtube Clone</h1>;
+    return (
+      <Grid justify="center" container spacing={16}>
+        <Grid item xs={12}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              {/* Search Bar */}
+            </Grid>
+            <Grid item xs={8}>
+              {/* Video Details */}
+            </Grid>
+            <Grid item xs={4}>
+              {/* Video List */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
