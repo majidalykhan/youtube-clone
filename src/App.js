80ebf074d866318105d77b2ba89c4b8c99b9ec37
Majid Ali Khan <51267704+majidalykhan@users.noreply.github.com>
2021-07-12 18:08:32 +0500

    Access api key

--- src/App.js
+++ src/App.js
@@ -6,14 +6,28 @@ import { SearchBar, VideoDetail, VideoList } from "./components";
 
 import youtube from "./api/youtube";
 
+const API_KEY = process.env.REACT_APP_YT_API_KEY;
+
 class App extends React.Component {
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: API_KEY,
+        q: searchTerm,
+      },
+    });
+    console.log(response);
+  };
+
   render() {
     return (
-      <Grid justify="center" container spacing={16}>
+      <Grid justify="center" container spacing={10}>
         <Grid item xs={12}>
-          <Grid container spacing={16}>
+          <Grid container spacing={10}>
             <Grid item xs={12}>
-              <SearchBar />
+              <SearchBar onFormSubmit={this.handleSubmit} />
             </Grid>
             <Grid item xs={8}>
               <VideoDetail />
